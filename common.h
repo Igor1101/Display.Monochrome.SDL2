52024d2997cf27db46530adbd777a0fad1e3f183
Igor <igor.muravyov.2015@gmail.com>
2020-01-06 13:20:56 +0200

    common staff finishing

--- common.h
+++ common.h
@@ -11,5 +11,11 @@
 #include <cstdio>
 
 #define pr_err(...)  fprintf(stderr, __VA_ARGS__); puts("")
+#define pr_info(...)  fprintf(stdout, __VA_ARGS__); puts("")
+#ifdef DEBUG
+#define pr_debug(...)  fprintf(stdout, __VA_ARGS__); puts("")
+#else
+#define pr_debug(...)
+#endif
 
 #endif /* COMMON_H_ */
